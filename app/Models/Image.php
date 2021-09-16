79c2078cf09855b550baf35e6f145532ec8fefa0
vndywhat <mau5d@yandex.ru>
2021-09-16 16:28:32 +0600

    added additional field for image model, added method to get a image url

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Image extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-      'file_name', 'imageable_bundle',
+        'file_name', 'imageable_bundle',
+    ];
+
+    protected $appends = [
+        'url'
     ];
 
     /**
@@ -19,5 +25,19 @@ class Image extends Model
     public function imageable(): \Illuminate\Database\Eloquent\Relations\MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function getUrlAttribute()
+    {
+        $imageableType = explode('\\', $this->imageable_type);
+        $entityDir = Str::plural(Str::lower(end($imageableType)));
+
+        $filePath = $entityDir . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->file_name;
+
+        if (Storage::disk('public')->exists($filePath)) {
+            return Storage::disk('public')->url($filePath);
+        }
+
+        return 'no-image';
     }
 }
